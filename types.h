1d435053bc117edb5fb6d85aa9542040fea72a3b
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2014-08-29 11:08:20 +0200

    Missing file

---
+++ types.h
@@ -0,0 +1,11 @@
+#include <stdint.h>
+#include <immintrin.h>
+
+typedef __m128i *contr;
+typedef uint32_t meter;
+typedef uint16_t place;
+typedef uint16_t agent;
+typedef uint16_t penny;
+typedef uint16_t edge;
+typedef uint32_t id;
+typedef float dist;
