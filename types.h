02ebdfdd7410f52aff1255e63abf5956295ea801
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2014-10-26 20:14:08 +0100

    64bit variables for agents

--- types.h
+++ types.h
@@ -4,7 +4,7 @@
 typedef __m128i *contr;
 typedef uint32_t meter;
 typedef uint16_t place;
-typedef uint32_t agent;
+typedef uint64_t agent;
 typedef uint32_t penny;
 typedef uint32_t edge;
 typedef uint32_t id;
