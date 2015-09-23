e1c3f4f46ca3c3fb2438f429072c9320571e2589
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2015-09-23 20:31:18 +0200

    Bug fixed

--- ratios.sh
+++ ratios.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-sort -t"," -k3 -n -o $1 $1
-sort -t"," -k3 -n -o $2 $2
+sort -t "," -k 3,3 -n -o $1 $1
+sort -t "," -k 3,3 -n -o $2 $2
 n=$1
 n=${n%%-*}
 n=${n##*/}
+#join -j 3 -t "," $1 $2 | awk -F "," '{print $5/$9}'
 echo $n,`join -j 3 -t "," $1 $2 | awk -F "," '{print $5/$9}' | awk -F "," '{ sum += $1; n++ } END { if (n > 0) print sum / n; }'`,`join -j 3 -t "," $1 $2 | awk -F "," '{print $5/$9}' | awk -F "," '{delta = $1 - avg; avg += delta / NR; mean2 += delta * ($1 - avg); } END { print sqrt(mean2 / NR); }'`
 
 
